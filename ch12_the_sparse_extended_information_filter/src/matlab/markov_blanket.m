99a1f05457dda961ae01d30c3917a8f898b6a94b
Pierre-Paul TACHER <ptacher@gmail.com>
2018-08-31 12:59:51 +0200

    maintain information graph using sparse adjacency matrix instead of matlab graph object.

--- ch12_the_sparse_extended_information_filter/src/matlab/markov_blanket.m
+++ ch12_the_sparse_extended_information_filter/src/matlab/markov_blanket.m
@@ -13,10 +13,10 @@ function [ mb ] = markov_blanket(n,m0)
     global G;
     
     mb1 = m0+1;
-       mb2 = find(G(n+1,:));
+    mb2 = find(G(n+1,:) | G(:,n+1)');
     
     if isempty(intersect_int(mb1,mb2))
-         mb = shortestpath(graph(G,'upper'),1,n+1);%%%%%%%%%%%%%%%this shoul not be empty
+         mb = bfs(n+1);%%%%%%%%%%%%%%%this shoul not be empty
          if ~isempty(mb)
              mb2 = union_int( mb2,mb);
          end
@@ -53,3 +53,29 @@ z = find(x & y);
 
 end
 
+function [ p ] = bfs(n)
+
+global G;
+sg=size(G,1);
+d=-ones(1,sg);
+q = zeros(1,sg);i=1;j=2;q(1)=1;
+found=false;
+p=zeros(1,0);
+curr=1;
+while ~found && i<j
+    curr=q(i);
+    i=i+1;
+    next=find(G(curr,:) & d==-1);
+    s=size(next,2);
+    q(j:j+s-1)=next;j=j+s;d(next)=curr*ones(1,s);
+    found=d(n)==curr;
+end
+if found
+    p  = curr;
+    prev=d(curr);
+    while prev~=1
+        p=[p prev];
+        prev=d(prev);
+    end
+end
+end
