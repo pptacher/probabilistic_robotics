cf58f51019385d3aa0836e7f546b1424e65994b4
Pierre-Paul TACHER <ptacher@gmail.com>
2018-09-10 17:50:34 +0200

    change in correspondence and in selection of active landmarks.

--- ch12_the_sparse_extended_information_filter/src/matlab/SEIF.m
+++ ch12_the_sparse_extended_information_filter/src/matlab/SEIF.m
@@ -6,58 +6,56 @@
 % =============================================================
 function SEIF
 
-    fileName1 = 'aa3_lsr2.mat' ;
-    fileName2 = 'aa3_dr.mat' ;
+fileName1 = 'aa3_lsr2.mat' ;
+fileName2 = 'aa3_dr.mat' ;
 
-%     
-%   file1 = 'landmarks_20000.mat' ;
-%   file2 = 'poses_20000.mat' ;
-%   file2b = 'poses_20000.mat' ;
-%   file3 = 'm0_20000.mat' ;
-%   file4 = 'xi_20000.mat' ;
-%   file5 = 'omega_20000.mat' ;
-%   file6 = 'G_20000.mat' ;
-% 
-%   
-    global AAr; 
-    AAr = (0:360)*pi/360 ;
-    dt = 25e-3;
-    Mask13 = uint16(2^13 -1) ;
-    
-    global G;
-    G=sparse(1,1);
-    
-    global  Q;
-    Q= diag([5 0.02]);
-    load(fileName1) ;
-    L = size(LASER) ; L=L(1) ;
-    timeLsr = double(TLsr) ; clear TLsr; j=1;
-    
-    load(fileName2) ;
-    
-    global m xi O
-    m=zeros(3,1);
-    xi=zeros(3,1);   
-   O=10e4*eye(3);
-   m0 = zeros(1,0);
-   
-    N = 20;%max active landmarks
 
-% figure(1) ;clf ; 
+%file1 = 'landmarks_20000.mat' ;
+%file2 = 'poses_20000.mat' ;
+%file2b = 'poses_20000.mat' ;
+%file3 = 'm0_20000.mat' ;
+%file4 = 'xi_20000.mat' ;
+%file5 = 'omega_20000.mat' ;
+%file6 = 'G_20000.mat' ;
+
+
+dt = 25e-3;
+Mask13 = uint16(2^13 -1) ;
+
+global G;
+G=sparse(1,1);
+
+global  Q;
+Q= diag([5 0.02]);
+load(fileName1) ;
+L = size(LASER);L=L(1) ;
+timeLsr = double(TLsr) ; clear TLsr; j=1;
+
+load(fileName2) ;
+
+global m xi O
+m=zeros(3,1);
+xi=zeros(3,1);
+O=10e4*eye(3);
+m0 = zeros(1,0);
+
+N = 20;%max active landmarks
+
+% figure(1) ;clf ;
 % zoom on ;
-% hold on;
-% hhh2=plot(G) ; 
-% axis([-50,50,0,75]);
-% hold off ;
+%hold on;
+%hhh2=plot(G) ;
+%axis([-50,50,0,75]);
+%hold off ;
 
-figure(1) ;clf ; 
-zoom on ;
-%hhh =plot(0,0,'.','erasemode','xor') ;   %laser
-hold on;
-hhh2=plot(0,0,'+','erasemode','xor','MarkerSize',2) ;  % landmarks centers
-hhh3=plot(0,0,'o','erasemode','xor','MarkerSize',2) ;   % approx. landm. circles
-axis([-200,200,-50,350]);
-hold off ;
+% figure(1) ;clf ;
+% zoom on ;
+% hhh =plot(0,0,'.','erasemode','xor') ;   %laser
+% hold on;
+% hhh2=plot(0,0,'+','erasemode','xor','MarkerSize',2) ;  % landmarks centers
+% hhh3=plot(0,0,'o','erasemode','xor','MarkerSize',2) ;   % approx. landm. circles
+% axis([-200,200,-50,350]);
+% hold off ;
 
 % G=importdata(file6);
 % O=importdata(file5);
@@ -66,7 +64,7 @@ hold off ;
 % m0=importdata(file3);
 % poses=importdata(file2);
 % poses1=importdata(file2b);
-% set(hhh2,'XData',[poses(1,:) poses1(1,:)],'YData',[poses(2,:) poses1(2,:)]) ;	        
+% set(hhh2,'XData',[poses(1,:) poses1(1,:)],'YData',[poses(2,:) poses1(2,:)]) ;
 % clearvars poses poses1;
 
 poses=zeros(3,5000);
@@ -75,67 +73,70 @@ pause(2);
 tstart1=tic;
 stindex=20000;
 
-    for i=stindex:size(time);
-        
-        j=find(timeLsr>=time(i),1);
-        tstart2=tic;
-
-        if i>stindex && mod(i,5000)==0
-            save(['poses_' num2str(i) '.mat'],'poses');
-            clearvars poses;
-            poses=zeros(3,5000);
-            
-            save(['landmarks_' num2str(i) '.mat'],'m');
-            save(['xi_' num2str(i) '.mat'],'xi');
-            save(['omega_' num2str(i) '.mat'],'O');
-            save(['m0_' num2str(i) '.mat'],'m0');
-            save(['G_' num2str(i) '.mat'],'G');
-        end
+for i=stindex:size(time);
     
-        SEIF_motion(speed(i),steering(i),dt,m0);
+    j=find(timeLsr>=time(i),1);
+    tstart2=tic;
     
-        SEIF_estimate(m0);
-
-         while j <= L && timeLsr(j) < time(i)+dt*1000 
-             m3 = zeros(1,0);c=zeros(1,0);
-             raw = double(  bitand( Mask13,LASER(j,:)) ) ;
-             raw = raw/100;
-             z = detectTreesI16(raw);
-             if ~isempty(z)
-                z = z(1:2,:);
-                c = SEIF_correspondence(z,m0);
-                SEIF_measurement(z,c);
-             end
-             j=j+1;
-             m3 = union(m3,c);
-             
-             if ~isempty(m3)
-                n=(size(xi,1)-3)/2;
-                q = union(m0,m3,'stable');
-                tmp=q(1,max(1,size(q,2)-N+1):end);
-                
-                m1 = setdiff(m0,tmp);
-                m1 = union(m1,setdiff(m3,tmp));
-                m0=tmp;
-                m2 = setdiff(setdiff(1:n,m1),m0);
-                if ~isempty(m1)              
-                    SEIF_sparsification(m0,m1,m2);
-                end
-             end
-         end
+    if i>stindex && mod(i,5000)==0
+        save(['poses_' num2str(i) '.mat'],'poses');
+        clearvars poses;
+        poses=zeros(3,5000);
         
-        titer1=toc(tstart1);titer2=toc(tstart2);clc;
-        fprintf('iter: %d\n', i); 
-        fprintf('iter time: %.5f\n',titer2 ); 
-        fprintf('avg: %.5f\n', titer1/(i-stindex+1));
-        poses(:,1+mod(i,5000))=m(1:3);
-        
-%         if mod(i,200)==0
-%             set(hhh3,'XData', m(2:2:end)','YData',m(3:2:end)') ;
-%         end
-%         title( num2str(i) );
-%         set(hhh2,'XData',[get(hhh2,'XData') m(1)],'YData',[get(hhh2,'YData') m(2)]) ;	
-%         drawnow ;
-
+        save(['landmarks_' num2str(i) '.mat'],'m');
+        save(['xi_' num2str(i) '.mat'],'xi');
+        save(['omega_' num2str(i) '.mat'],'O');
+        save(['m0_' num2str(i) '.mat'],'m0');
+        save(['G_' num2str(i) '.mat'],'G');
     end
+    
+    SEIF_motion(speed(i),steering(i),dt,m0);
+    
+    SEIF_estimate(m0);
+    
+    while j <= L && timeLsr(j) < time(i)+dt*1000
+        m3 = zeros(1,0);c=zeros(1,0);
+        raw = double(  bitand( Mask13,LASER(j,:)) ) ;
+        raw = raw/100;
+        z = detectTreesI16(raw);
+        if ~isempty(z)
+            z = z(1:2,:);
+            c = SEIF_correspondence(z,m0);
+            SEIF_measurement(z,c);
+        end
+        j=j+1;
+        m3 = union(m3,c);
+        
+        if ~isempty(m3)
+            n=(size(xi,1)-3)/2;
+            
+            m0a = setdiff(m0,m3);
+            q = [m0a unique(m3)];
+            
+            tmp=q(1,max(1,size(q,2)-N+1):end);
+            
+            m1 = setdiff(m0,tmp);
+            m1 = union(m1,setdiff(m3,tmp));
+            m0=tmp;
+            m2 = setdiff(setdiff(1:n,m1),m0);
+            if ~isempty(m1)
+                SEIF_sparsification(m0,m1,m2);
+            end
+        end
+    end
+    
+    titer1=toc(tstart1);titer2=toc(tstart2);clc;
+    fprintf('iter: %d\n', i);
+    fprintf('iter time: %.5f\n',titer2 );
+    fprintf('avg: %.5f\n', titer1/(i-stindex+1));
+    poses(:,1+mod(i,5000))=m(1:3);
+    
+    %         if mod(i,200)==0
+    %             set(hhh3,'XData', m(2:2:end)','YData',m(3:2:end)') ;
+    %         end
+    %         title( num2str(i) );
+    %         set(hhh2,'XData',[get(hhh2,'XData') m(1)],'YData',[get(hhh2,'YData') m(2)]) ;
+    %         drawnow ;
+    
+end
 end
--- ch12_the_sparse_extended_information_filter/src/matlab/SEIF_correspondence.m
+++ ch12_the_sparse_extended_information_filter/src/matlab/SEIF_correspondence.m
@@ -10,109 +10,115 @@
 % =============================================================
 
 function [ c ] = SEIF_correspondence( z,m0)
+
+
+global G;global Q;
+
+global m xi O
+k = size(z,2);
+if k==0
+    c=zeros(1,0);
+    return;
+end
+c= zeros(1,k);
+n = (size(xi,1)-3)/2;
+
+if n > 0
     
-
-    global G;global Q;
-
-    global m xi O
-    k = size(z,2);
-    if k==0
-        c=zeros(1,0);
-        return;
-    end
-    c= zeros(1,k);
-    n = (size(xi,1)-3)/2;
+    %rough landmarks selection
+    t = m(4:end)';t1=t(1:2:end);t2=t(2:2:end);
+    lst = find(abs(t1-m(1))<=75 ...
+        & abs(t2-m(2))<=75 ...
+        & (t1-m(1))*cos(m(3))+(t2-m(2))*sin(m(3))>=-5);
+    a=size(lst,2);
+    A = zeros(2*a,2);
     
-    if n > 0
-   
-        %rough landmarks selection
-        t = m(4:end)';t1=t(1:2:end);t2=t(2:2:end);
-        lst = find(abs(t1-m(1))<=75 ...
-            & abs(t2-m(2))<=75 ...
-            & (t1-m(1))*cos(m(3))+(t2-m(2))*sin(m(3))>=-5);
-        a=size(lst,2);
-        A = zeros(2*a,2);
-       
-        J = jacobian_measurement(m,lst);
-        zhat = reshape(equation_measurement(m,lst),2,1,a);
+    J = jacobian_measurement(m,lst);
+    zhat = reshape(equation_measurement(m,lst),2,1,a);
     
-        for ii=1:a
-            
-                ind = markov_blanket(lst(ii),m0);
-                
-                ind=ind(2:end);
-                c1 = 2 + 2*ind;c2=c1+1;ind=reshape([c1;c2],[1,2*size(ind,2)]);
-            
-                ind1 = find(ind==2+2*lst(ii));
-
-                F=Fxm(ind1,3+size(ind,2));
-                S = J(:,:,ii)*F/O([1:3 ind],[1:3 ind]);
-
-                [U,M1]=eig(S*F'*J(:,:,ii)'+Q);
-                U = diag(sqrt(1./diag(M1)))*U';
-                A(2*ii-1:2*ii,:) = U;
-                
-                clear F;
-                   
-        end
-
-        F = reshape(permute(bsxfun(@minus,zhat,z),[1,3,2]),2*a,k);
-        F(2:2:end,:)=measure(F(2:2:end,:));
-        %first pass over active landmarks        
-        N = zeros(2*a,k);
-        [C,ia,~] = intersect(lst,m0,'stable');
+    for ii=1:a
         
-        for j=1:size(C,2)
-            jj=ia(j);
-            N(2*jj-1:2*jj,:)= A(2*jj-1:2*jj,:)*F(2*jj-1:2*jj,:);
-        end
-        N1 = N(reshape([2*ia'-1;2*ia'],1,[]),:);
-        N1 = N1.^2;M2=N1(2:2:end,:);N1 = N1(1:2:end,:)+M2;
-        [m1, c] = min(N1,[],1);%search min along first dim, even if row vector.
+        ind = markov_blanket(lst(ii),m0);
         
-        new = find(m1>chi2inv(0.5,2));
-        new1 = find(m1(new)<chi2inv(0.95,2));ia=ia(setdiff(1:size(ia,1),c(new(new1)),'stable'));       
-        c = C(1,c);
-        %second pass over left over landmarks      
-        if (~isempty(new) && size(C,2)<a) || isempty(C)
-            if isempty(C)
-                new=1:k;
-            end
-            ib=setdiff(1:a,ia,'stable');%ib is a row
-            lst = lst(1,ib);
-            for j=1:size(lst,2)
-                jj=ib(j);
-                N(2*jj-1:2*jj,new)= A(2*jj-1:2*jj,:)*F(2*jj-1:2*jj,new);
-            end
-            if ~isempty(ib)
-                N1 = N(reshape([2*ib-1;2*ib],1,[]),new);
-                N1 = N1.^2;M2=N1(2:2:end,:);N1 = N1(1:2:end,:)+M2;
-                [m2, c(new)] = min(N1,[],1);%search min along first dim, even if row vector.
-                c(new) = lst(1,c(new));
-                new = new(1,find(m2>chi2inv(0.95,2)));
-            end
+        ind=ind(2:end);
+        c1 = 2 + 2*ind;c2=c1+1;ind=reshape([c1;c2],[1,2*size(ind,2)]);
+        
+        ind1 = find(ind==2+2*lst(ii));
+        
+        F=Fxm(ind1,3+size(ind,2));
+        S = J(:,:,ii)*F/O([1:3 ind],[1:3 ind]);
+        
+        [U,M1]=eig(S*F'*J(:,:,ii)'+Q);
+        U = diag(sqrt(1./diag(M1)))*U';
+        A(2*ii-1:2*ii,:) = U;
+        
+        clear F;
+        
+    end
+    
+    F = reshape(permute(bsxfun(@minus,zhat,z),[1,3,2]),2*a,k);
+    F(2:2:end,:)=measure(F(2:2:end,:));
+    %first pass over active landmarks
+    N = zeros(2*a,k);
+    [C,ia,~] = intersect(lst,m0,'stable');
+    
+    for j=1:size(C,2)
+        jj=ia(j);
+        N(2*jj-1:2*jj,:)= A(2*jj-1:2*jj,:)*F(2*jj-1:2*jj,:);
+    end
+    N1 = N(reshape([2*ia'-1;2*ia'],1,[]),:);
+    N1 = N1.^2;M2=N1(2:2:end,:);N1 = N1(1:2:end,:)+M2;
+    [m1, c] = min(N1,[],1);%search min along first dim, even if row vector.
+    
+    new = find(m1>chi2inv(0.5,2));
+    new1 = find(m1(new)<chi2inv(0.95,2));ia=ia(setdiff(1:size(ia,1),c(new(new1)),'stable'));
+    c = C(1,c);
+    %second pass over left over landmarks
+    if (~isempty(new) && size(C,2)<a) || isempty(C)
+        if isempty(C)
+            new=1:k;
         end
-    else
-        new = 1:k;
+        ib=setdiff(1:a,ia,'stable');%ib is a row
+        lst = lst(1,ib);
+        for j=1:size(lst,2)
+            jj=ib(j);
+            N(2*jj-1:2*jj,new)= A(2*jj-1:2*jj,:)*F(2*jj-1:2*jj,new);
+        end
+        if ~isempty(ib)
+            N1 = N(reshape([2*ib-1;2*ib],1,[]),new);
+            N1 = N1.^2;M2=N1(2:2:end,:);N1 = N1(1:2:end,:)+M2;
+            [m2, c(new)] = min(N1,[],1);%search min along first dim, even if row vector.
+            c(new) = lst(1,c(new));
+            new = new(1,find(m2>chi2inv(0.95,2)));
+            new1=zeros(1,0);
+        end
     end
-    if ~isempty(new)
-        c(1,new) = n + (1:(size(new,2)));
-    end
+else
+    new = 1:k;
+    new1=zeros(1,0);
+end
+if ~isempty(new1)
+    new = new(setdiff(1:size(new,2),new1));
+end
+
+if ~isempty(new)
+    c(1,new) = n + (1:(size(new,2)));
+end
 
 end
 
 function alpha = measure(theta)
-    tmp =mod(theta,2*pi);
-    %matlab mod may return 2pi due to precision limitation
-    alpha = tmp-min(floor(tmp/pi),1)*2*pi;
+tmp =mod(theta,2*pi);
+%matlab mod may return 2pi due to precision limitation
+alpha = tmp-min(floor(tmp/pi),1)*2*pi;
 end
 
 function [ F ] =Fxm(p,n)
 
-    if p>0
-        F=sparse(1:5, [1 2 3 3+p 4+p],ones(1,5),5,n);
-    else if n==0
-            F=sparse(1:3,1:3,ones(1,3),3,n);
-        end
+if p>0
+    F=sparse(1:5, [1 2 3 3+p 4+p],ones(1,5),5,n);
+else if n==0
+        F=sparse(1:3,1:3,ones(1,3),3,n);
     end
+end
 end
