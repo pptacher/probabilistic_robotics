cb042ea249d971bf82d43fa293c18df5505df5ad
Pierre-Paul TACHER <ptacher@gmail.com>
2018-10-15 23:59:23 +0200

    enforce use of solver for symmetric matrices

--- ch12_the_sparse_extended_information_filter/src/julia/sparsification.jl
+++ ch12_the_sparse_extended_information_filter/src/julia/sparsification.jl
@@ -17,13 +17,13 @@ function sparsification(m₀,m₁,μ,ξ,Ω,graph)
     ind₁ = vcat(ind₁...)
 
     Ω₂ = Ω[[1:3;ind₀;ind₁],ind₁]
-    l₁ = Ω₂ / Ω[ind₁,ind₁] * Ω₂'
+    l₁ = Ω₂ / Symmetric(Ω[ind₁,ind₁]) * Ω₂'
 
     Ω₃ = Ω[[1:3;ind₀;ind₁],[1:3;ind₁]]
-    l₂ = Ω₃ / Ω[[1:3;ind₁],[1:3;ind₁]] * Ω₃'
+    l₂ = Ω₃ / Symmetric(Ω[[1:3;ind₁],[1:3;ind₁]]) * Ω₃'
 
     Ω₄ = Ω[:,1:3]
-    l₃ = Ω₄ / Ω[1:3,1:3] * Ω₄'
+    l₃ = Ω₄ / Symmetric(Ω[1:3,1:3]) * Ω₄'
 
     Ω₁ = copy(Ω)
     Ω[[1:3;ind₀;ind₁],[1:3;ind₀;ind₁]] .-= l₁
